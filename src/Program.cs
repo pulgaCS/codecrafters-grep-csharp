fdf8e59445516fdc008099a5b0c25aaddbd38fd8
pulga <goncalo94sousa@gmail.com>
2024-06-06 00:45:30 +0100

    pass stage

--- src/Program.cs
+++ src/Program.cs
@@ -1,56 +1,85 @@
 using System;
-using System.IO;
+using System.Linq;
 
-static bool MatchPattern(string inputLine, string pattern) {
-    return MatchPatternRecursive(inputLine, 0, pattern, 0);
+public class Pattern {
+    public const string DIGIT = @"\d";
+    public const string ALNUM = @"\w";
 }
 
-static bool MatchPatternRecursive(string inputLine, int inputIndex, string pattern, int patternIndex) {
-    // If we have reached the end of the pattern, check if we have also reached the end of the input
-    if (patternIndex == pattern.Length) {
-        return inputIndex == inputLine.Length;
-    }
-
-    // If the next pattern character is an escaped character class
-    if (pattern[patternIndex] == '\\' && patternIndex + 1 < pattern.Length) {
-        char patternClass = pattern[patternIndex + 1];
-        if (inputIndex < inputLine.Length && MatchCharacterClass(inputLine[inputIndex], patternClass)) {
-            return MatchPatternRecursive(inputLine, inputIndex + 1, pattern, patternIndex + 2);
+public class Program {
+    public static bool MatchPattern(string inputLine, string pattern) {
+        if (pattern.Length == 1) {
+            return inputLine.Contains(pattern);
         }
-        return false;
+        else if (pattern == Pattern.DIGIT) {
+            return inputLine.Any(char.IsDigit);
+        }
+        else if (pattern == Pattern.ALNUM) {
+            return inputLine.Any(char.IsLetterOrDigit);
+        }
+        if (inputLine.Length == 0 && pattern.Length == 0) {
+            return true;
+        }
+        if (string.IsNullOrEmpty(pattern)) {
+            return true;
+        }
+        if (string.IsNullOrEmpty(inputLine)) {
+            return false;
+        }
+        if (pattern[0] == inputLine[0]) {
+            return MatchPattern(inputLine.Substring(1), pattern.Substring(1));
+        }
+        else if (pattern.StartsWith(Pattern.DIGIT)) {
+            for (int i = 0; i < inputLine.Length; i++) {
+                if (char.IsDigit(inputLine[i])) {
+                    return MatchPattern(inputLine.Substring(i), pattern.Substring(2));
+                }
+            }
+            return false;
+        }
+        else if (pattern.StartsWith(Pattern.ALNUM)) {
+            if (char.IsLetterOrDigit(inputLine[0])) {
+                return MatchPattern(inputLine.Substring(1), pattern.Substring(2));
+            }
+            else {
+                return false;
+            }
+        }
+        else if (pattern[0] == '[' && pattern[pattern.Length - 1] == ']') {
+            if (pattern[1] == '^') {
+                var chrs = pattern.Substring(2, pattern.Length - 3).ToCharArray();
+                foreach (var c in chrs) {
+                    if (inputLine.Contains(c)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            var characters = pattern.Substring(1, pattern.Length - 2).ToCharArray();
+            foreach (var c in characters) {
+                if (inputLine.Contains(c)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else {
+            throw new InvalidOperationException($"Unhandled pattern: {pattern}");
+        }
     }
 
-    // Otherwise, check for direct character match
-    if (inputIndex < inputLine.Length && inputLine[inputIndex] == pattern[patternIndex]) {
-        return MatchPatternRecursive(inputLine, inputIndex + 1, pattern, patternIndex + 1);
+    public static void Main(string[] args) {
+        if (args.Length < 3 || args[0] != "-E") {
+            Console.WriteLine("Expected first argument to be '-E'");
+            Environment.Exit(1);
+        }
+        string pattern = args[2];
+        string inputLine = Console.In.ReadToEnd();
+        if (MatchPattern(inputLine, pattern)) {
+            Environment.Exit(0);
+        }
+        else {
+            Environment.Exit(1);
+        }
     }
-
-    // If current characters do not match and we are not at the end of pattern/input, return false
-    return false;
-}
-
-static bool MatchCharacterClass(char c, char patternClass) {
-    return patternClass switch {
-        'd' => char.IsDigit(c),
-        'w' => char.IsLetterOrDigit(c),
-        _ => throw new ArgumentException($"Unknown character class: \\{patternClass}")
-    };
-}
-
-if (args.Length < 2 || args[0] != "-E") {
-    Console.WriteLine("Expected first argument to be '-E'");
-    Environment.Exit(2);
-}
-
-string pattern = args[1];
-string inputLine = Console.In.ReadToEnd().Trim(); // Trim to remove any extra newline from input
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-if (MatchPattern(inputLine, pattern)) {
-    Environment.Exit(0);
-}
-else {
-    Environment.Exit(1);
 }
